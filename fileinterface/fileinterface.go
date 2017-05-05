fb0db1c1e4101a2b3864d4f89e9d7e2650c6e59b
Ulrich Hoffmann <uho@xlerb.de>
2017-05-05 07:05:27 +0200

    Provide simple sample implementation for fileinterface

--- fileinterface/fileinterface.go
+++ fileinterface/fileinterface.go
@@ -4,32 +4,52 @@ package fileinterface
 
 import (
 	"errors"
+	"os"
 )
 
 const Blocksize = 4096
+
+const MAXOPENFILES = 16
 
 type FID int // File ID
 
 type Block [Blocksize]byte
 
+var files [MAXOPENFILES]*os.File
+var lastFid = -1
+
 // Creates a new file with a given name. Return the FID and nil if succesful.
 // The file is then open for reading and writing.
 // If unsuccessful, return any FID and an error value describing the error.
 func Create(name string) (FID, error) {
-	return 0, errors.New("not implemented")
+	file, err := os.Create(name)
+	if err != nil {
+		return 0, err
+	} else {
+		lastFid++
+		files[lastFid] = file
+		return FID(lastFid), nil
+	}
 }
 
 // Deletes a file with a given name. Return nil if succesful.
 // If unsuccessful, return an error value describing the error.
 func Delete(name string) error {
-	return errors.New("not implemented")
+	return os.Remove(name)
 }
 
 // Opens a file with a given name for reading and writing. Return nil if succesful.
 // If unsuccessful, return  any FID and an error value describing the error.
 // Possible errors include FileNotFoundError oder FileAlreadyOpenError
 func Open(name string) (FID, error) {
-	return 0, errors.New("not implemented")
+	file, err := os.OpenFile(name, os.O_RDWR, 0644)
+	if err != nil {
+		return 0, err
+	} else {
+		lastFid++
+		files[lastFid] = file
+		return FID(lastFid), nil
+	}
 }
 
 // Length calculates the number of blocks available in the file given by fileNo. Return nil if succesful.
@@ -44,7 +64,16 @@ func Length(fileNo FID) (int, error) {
 // If unsuccessful, return nil and an error value describing the error.
 // Possible errors include FileNotOpenError
 func Read(fileNo FID, blockNo int) (*Block, error) {
-	return &Block{}, errors.New("not implemented")
+	var block Block
+if file, err := checkOpenFile(fileNo); err != nil {
+		return nil, err
+	} else if _,err:=file.Seek(int64(blockNo*Blocksize),0); err != nil {
+			return nil, err
+	} else if _,err:=file.Read([]byte((block)[:])); err != nil {
+		    return nil,err
+	} else {
+	  return &block, nil
+    }
 }
 
 // Writes the block given by the pointer block to the block number blockNo in
@@ -52,15 +81,39 @@ func Read(fileNo FID, blockNo int) (*Block, error) {
 // Return nil if succesful.
 // If unsuccessful, return an error value describing the error.
 // Possible errors include FileNotOpen or WriteError
-func Write(fileNo FID, blockNo int, block *Block) error { //  FileNotOpenException, IOException;
-	return errors.New("not implemented")
+func Write(fileNo FID, blockNo int, block *Block) error {
+if file, err := checkOpenFile(fileNo); err != nil {
+		return err
+	} else if _,err:=file.Seek(int64(blockNo*Blocksize),0); err != nil {
+			return err
+	} else if _,err:=file.Write([]byte((*block)[:])); err != nil {
+		    return err
+	} else {
+	  return nil
+    }
 }
 
 // Close the file given by fileNo. Return nil if succesful.
 // If unsuccessful, return an error value describing the error.
 // Possible errors include FileNotOpenError
 func Close(fileNo FID) error {
-	return errors.New("not implemented")
+	if file, err := checkOpenFile(fileNo); err != nil {
+		return err
+	} else {
+		return file.Close()
+	}
+}
+
+func checkOpenFile(fileNo FID) (*os.File, error) {
+	f := int(fileNo)
+	if f >= MAXOPENFILES {
+		return nil, errors.New("illegal FID")
+	}
+	file := files[f]
+	if file == nil {
+		return nil, errors.New("file not open")
+	}
+	return file, nil
 }
 
 // Return a list of possible file names
--- fileinterface/fileinterface_test.go
+++ fileinterface/fileinterface_test.go
@@ -1,11 +1,14 @@
 package fileinterface
 
-import "testing"
+import (
+  "testing"
+  "errors"
+  )
 
 func TestCreate(t *testing.T) {
 	var f FID
 	var err error
-	if f, err = Open("dummyfile"); err != nil {
+	if f, err = Create("dummyfile"); err != nil {
 		t.Error(err)
 	} else {
 		if err = Close(f); err != nil {
@@ -15,18 +18,52 @@ func TestCreate(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
+	err := Delete("dummyfile")
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestOpen(t *testing.T) {
+	if f, err := Open("sample.database"); err != nil {
+		t.Error(err)
+	} else if err = Close(f); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestRead(t *testing.T) {
+	var block *Block
+	if f, err := Open("sample.database"); err != nil {
+		t.Error(err)
+	} else if block, err = Read(f, 5); err != nil {
+		t.Error(err)
+	} else if err = Close(f); err != nil {
+		t.Error(err)
+	}
+	for i:=0; i<Blocksize; i++ {
+		if block[i] != byte(i & 0xFF) {
+			t.Error(errors.New("block has unexpected value"))
+			break
+		}
+	}
 }
 
 func TestLength(t *testing.T) {
 }
 
 func TestWrite(t *testing.T) {
+	var block Block
+	for i:=0; i<Blocksize; i++ {
+		block[i]= byte(i & 0xFF)
+	}
+	if f, err := Open("sample.database"); err != nil {
+		t.Error(err)
+	} else if err := Write(f, 5, &block); err != nil {
+		t.Error(err)
+	} else if err = Close(f); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestLS(t *testing.T) {

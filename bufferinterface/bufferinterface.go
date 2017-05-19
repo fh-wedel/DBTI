61df49a6d16dfef8d4baf10c27c64409fdf61117
Ulrich Hoffmann <uho@xlerb.de>
2017-05-19 08:08:01 +0200

    Provide linear buffer search

--- bufferinterface/bufferinterface.go
+++ bufferinterface/bufferinterface.go
@@ -1,20 +1,55 @@
 // Package fileinterface provides access to the underlying operating system files.
 // Files are organized in blocks.
-package fileinterface
+package bufferinterface
 
 import (
 	"errors"
 	"github.com/fh-wedel/DBTI/fileinterface"
+	"math/rand"
 )
 
 const PageSize = fileinterface.Blocksize
 
 type Page [PageSize]byte
 
+const Buffersize = 16
+
+var Buffer [Buffersize]Page
+
+func searchBuffer(pageNo int) int {
+	for i := 0; i < Buffersize; i++ {
+		b := &Buffer[i]
+		var no int = (int(b[0])*0x01000000 + int(b[1])*0x00010000 + int(b[2])*0x00000100 + int(b[3])*0x00000001)
+		if no == pageNo {
+			return i
+		}
+	}
+	return -1
+}
+
+func allocateNewFrame(pageNo int) int {
+	no := rand.Intn(Buffersize)
+	b := &Buffer[no]
+	// Record pageno
+	b[0] = byte((pageNo & 0xFF000000) >> 24)
+	b[1] = byte((pageNo & 0x00FF0000) >> 16)
+	b[2] = byte((pageNo & 0x0000FF00) >> 8)
+	b[3] = byte((pageNo & 0x000000FF))
+	return no
+}
+
 // Request page with number pageNo. Returns pointer to page data in system buffer (and err is nil)
 // If unsuccessful, return nil and an error value describing the error.
-func Request(PageNo int) (*Page, error) {
-	return nil, errors.New("not implemented")
+func Request(pageNo int) (*Page, error) {
+	var nr int = searchBuffer(pageNo)
+	if nr == -1 {
+		nr = allocateNewFrame(pageNo)
+		if nr == -1 {
+			return nil, errors.New("Buffer exhausted!")
+		}
+	}
+
+	return &Buffer[nr], nil
 }
 
 // Mark the page pageNo as pinned. It's pointer will stay valid and the page
--- bufferinterface/bufferinterface_test.go
+++ bufferinterface/bufferinterface_test.go
@@ -1,8 +1,33 @@
-package fileinterface
+package bufferinterface
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+)
 
 func TestRequest(t *testing.T) {
+	p1, err := Request(12)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if p1 == nil {
+		t.Error("got nil")
+		return
+	}
+	p2, err := Request(12)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if p2 == nil {
+		t.Error("got nil")
+		return
+	}
+	if p1 != p2 {
+		t.Error("requested addresses do not match: ", unsafe.Pointer(p1), ", ", unsafe.Pointer(p2))
+	}
+
 }
 
 func TestFix(t *testing.T) {

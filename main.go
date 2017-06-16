d4f4b1da9acafb878fa9c2fd2c2034e74003a677
Ulrich Hoffmann <uho@xlerb.de>
2017-06-16 07:55:22 +0200

    Procide interaction

--- main.go
+++ main.go
@@ -3,9 +3,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/fh-wedel/DBTI/bufferinterface"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("Data Base Theory and Implementation Server started\n")
+	fmt.Printf("Data Base Theory and Implementation Interaction\n")
+	fmt.Println()
+
+	terminal := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("DBTI> ")
+		cmd, _ := terminal.ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+
+		if strings.HasPrefix(cmd, "help") {
+			fmt.Printf("Data Base Theory and Implementation Interaction\n")
+			fmt.Println()
+			fmt.Printf("help\t\tThis help\n")
+			fmt.Printf("quit\t\tExit interaction\n")
+			fmt.Printf("page <n>\tShow page n\n")
+			fmt.Println()
+			continue
+		}
+
+		if strings.HasPrefix(cmd, "quit") {
+			break
+		}
+
+		if strings.HasPrefix(cmd, "page") {
+			pageNo, err := strconv.Atoi(strings.Split(cmd, " ")[1])
+			if err != nil {
+				fmt.Println(err.Error)
+				continue
+			}
+			p, err := bufferinterface.Request(pageNo)
+			if err != nil {
+				fmt.Printf(err.Error())
+			} else {
+				for i := 0; i < bufferinterface.PageSize; i++ {
+					fmt.Printf("%02X ", p[i])
+					if (i+1)%32 == 0 {
+						fmt.Println()
+					}
+				}
+			}
+			continue
+		}
+
+		fmt.Printf("Unknown command '%s'. Try help\n", cmd)
+	}
 }
